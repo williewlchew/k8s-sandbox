db3be17b18d47cb8f5d0a92bffdd0e100dafbf21
Willie Chew <37847148+williewlchew@users.noreply.github.com>
2023-07-11 14:24:45 -0600

    Delete sample-minikube-deploy directory

--- sample-minikube-deploy/README.md
+++
@@ -1,1 +0,0 @@
-followed https://dev.to/chefgs/deploy-kubernetes-resources-in-minikube-cluster-using-terraform-1p8o 
--- sample-minikube-deploy/verify.sh
+++
@@ -1,6 +0,0 @@
-# ideally run with verbose
-kubectl get ns 
-
-kubectl get deployment -n k8s-ns-by-tf
-
-kubectl get pods -n k8s-ns-by-tf
