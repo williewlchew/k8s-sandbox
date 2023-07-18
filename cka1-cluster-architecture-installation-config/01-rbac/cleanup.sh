6933deaeb98a67848a053f43e01a706b1486a518
williewlchew <williewlchew@gmail.com>
2023-07-18 14:36:53 -0600

    cleanup

---
+++ cka1-cluster-architecture-installation-config/01-rbac/cleanup.sh
@@ -0,0 +1,8 @@
+
+kubectl config delete-context eng-context
+
+kubectl delete -f myapp.yaml
+kubectl delete -f myrolebinding.yaml
+kubectl delete -f myrole.yaml
+kubectl delete -f mynamespace.yaml
+
